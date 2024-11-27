197c835c26eb31b115d4834ddf4b8f5a23d0f5a0
CaseyBartman <caseywilliambartman@gmail.com>
2024-11-27 18:52:37 -0500

    Added validation to make sure the date of an expense falls within date range

--- splitthebill/app/models/expense.rb
+++ splitthebill/app/models/expense.rb
@@ -9,4 +9,33 @@ class Expense < ApplicationRecord
   validates :name, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
   validates :date, presence: true
+
+  # Custom validation to ensure date falls within trip range!
+  validate :date_within_trip_range
+
+  # # Custom validation to check if the sum of contributions equals the expense amount
+  # validate :contributions_sum_to_amount
+
+  private
+
+  #Also has a custom error message to give the user more info!
+  def date_within_trip_range
+    if trip.present? && date.present?
+      unless date >= trip.start_date && date <= trip.end_date
+        errors.add(:date, "must be between the trip's start date (#{trip.start_date}) and end date (#{trip.end_date})")
+      end
+    end
+  end
+
+
+  # I HOPE I CAN GET THIS TO WORK SOON!
+  # def contributions_sum_to_amount
+  #   # Calculate the sum of all contribution amounts
+  #   total_contributions = contributions.sum(:amount)
+
+  #   # Check if the total contribution amount matches the expense's amount
+  #   if total_contributions != amount
+  #     errors.add(:base, "Total of contributions (#{total_contributions}) must equal the expense amount(#{amount}).")
+  #   end
+  # end
 end
