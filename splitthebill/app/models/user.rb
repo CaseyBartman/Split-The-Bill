79c691664575343d288200be4f1c7e721e421c53
Jemin <jemin@MSI>
2024-11-27 11:22:28 -0500

    authentication routes working. Still need to include in nav bar and style

--- splitthebill/app/models/user.rb
+++ splitthebill/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :expenses, through: :contributions
   has_many :participants, dependent: :destroy
   has_many :trips, through: :participants
+
+  # Include default Devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        
 end
--- splitthebill/config/routes.rb
+++ splitthebill/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :expenses
   resources :trips
   resources :home
+
+  # Devise routes
+  devise_for :users
+  
   resources :users, only: [:index, :show, :new, :create] # Note, I left out a few!
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
