46b4ed2e0444bba7cbfeec7120c71c2ae07d2a05
Jemin <jemin@MSI>
2024-12-01 15:15:01 -0500

    added the devise line

--- splitthebill/config/routes.rb
+++ splitthebill/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :home
   resources :users, only: [:index, :show, :new, :create] # Note, a few are left out, because we use Devise
 
+  devise_for :users
+
    #Custom route for marking an individual contribution as paid!
    patch 'contributions/:id/mark_paid', to: 'contributions#mark_paid', as: 'mark_contribution_paid'
 
