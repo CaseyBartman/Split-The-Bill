008dd79ef72d772d41c7c2c8ef1cad83f6a84d93
CaseyBartman <caseywilliambartman@gmail.com>
2024-12-03 17:04:57 -0500

    Added fixtures from seed data, adjusted participant model, added test cases for trips, users, participants

--- splitthebill/app/models/participant.rb
+++ splitthebill/app/models/participant.rb
@@ -1,4 +1,8 @@
 class Participant < ApplicationRecord
   belongs_to :trip
   belongs_to :user
+
+  validates :trip, presence: true
+  validates :user, presence: true
+  validates :user, uniqueness: { scope: :trip, message: "has already been taken" }
 end
--- splitthebill/test/models/user_test.rb
+++ splitthebill/test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(
+      name: "Test User",
+      email: "user@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test "invalid without name" do
+    @user.name = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:name], "can't be blank"
+  end
+
+  test "invalid without email" do
+    @user.email = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:email], "can't be blank"
+  end
+
+  test "invalid with mismatched password confirmation" do
+    @user.password_confirmation = "mismatch"
+    assert_not @user.valid?
+    assert_includes @user.errors[:password_confirmation], "doesn't match Password"
+  end
 end
