06c65b382a901c92538854c94e426373f241a67e
Jemin <jemin@MSI>
2024-12-07 13:09:35 -0500

    got a small flash for loggin in/out

--- splitthebill/app/controllers/application_controller.rb
+++ splitthebill/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Devise will redirect to the root_path after sign-out
   def after_sign_out_path_for(resource_or_scope)
     reset_session # Clear session data
+    flash[:notice] = "You have successfully logged out."
     root_path # or the path you want the user to go to after sign out
   end
 
