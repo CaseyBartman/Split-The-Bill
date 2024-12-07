12368c2c299bcb17c05f9af24c8f2600d46131fa
Jemin <jemin@MSI>
2024-12-07 11:42:08 -0500

    Ensures user must check ToS before creating an account

--- splitthebill/app/javascript/application.js
+++ splitthebill/app/javascript/application.js
@@ -8,26 +8,31 @@ import "@popperjs/core" // Required for tooltips and popovers
 
 start()
 
-document.addEventListener('DOMContentLoaded', () => {
-    const signOutLink = document.getElementById('sign-out-link');
-    if (signOutLink) {
-      signOutLink.addEventListener('click', function(e) {
-        e.preventDefault(); // Prevent the default link behavior (GET request)
-        fetch(signOutLink.href, {
-          method: 'DELETE',
-          headers: {
-            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-          }
-        })
-        .then(response => {
-          if (response.ok) {
-            window.location.href = '/'; // Redirect to the root or any other page after sign out
-          } else {
-            console.error('Sign-out failed');
-          }
-        })
-        .catch(error => console.error('Error signing out:', error));
-      });
-    }
-  });
+  document.addEventListener('DOMContentLoaded', () => {
+      const signOutLink = document.getElementById('sign-out-link');
+      if (signOutLink) {
+        signOutLink.addEventListener('click', function(e) {
+          e.preventDefault(); // Prevent the default link behavior (GET request)
+          fetch(signOutLink.href, {
+            method: 'DELETE',
+            headers: {
+              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+            }
+          })
+          .then(response => {
+            if (response.ok) {
+              window.location.href = '/'; // Redirect to the root or any other page after sign out
+            } else {
+              console.error('Sign-out failed');
+            }
+          })
+          .catch(error => console.error('Error signing out:', error));
+        });
+      }
+    });
+
+
+
+
+
   
