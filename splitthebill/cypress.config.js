11c40ee3e974452312724a627793652aceca5a74
CaseyBartman <caseywilliambartman@gmail.com>
2025-02-01 18:25:07 -0500

    Added initial loading test... bug with dependencies and cypress

--- splitthebill/cypress.config.js
+++ splitthebill/cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   e2e: {
+    specPattern: 'cypress/**/*.js', //Now, it looks for the tests we need
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- splitthebill/cypress/e2e/3-first-examples/initialHomepageTests.js
+++ splitthebill/cypress/e2e/3-first-examples/initialHomepageTests.js
@@ -1,9 +1,14 @@
-//reference types="cypress" /> //Helps our code editor see that we use cypress just to help with autocompletion for commands
+///<reference types="cypress" /> 
+//^Helps our code editor see that we use cypress just to help with autocompletion for commands
 
 //Describe is used to order related tests together. We can think of it like a folder
 //Here we could separate homepage tests from other tests, like login tests, for example
 describe("Initial Homepage Tests", () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
-      })
+    // it('Does not do much!', () => {
+    //     expect(true).to.equal(true)
+    //   })
+    it ('Clicks the "Login" view on the first page and checks if the URL is correct',()=> {
+        cy.visit('http://localhost:3000')
+        cy.get('[id^=SignIn]').click() //cy.get(selector, this chooses the element that has an id starting with "SignIn)
+        cy.url().should('include','/login')})
 })
